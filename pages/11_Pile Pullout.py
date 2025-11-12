41cf189e4f702fb8e121f5348ae29edc49ed7533
ptcypress <cmitra86@gmail.com>
2025-11-12 13:35:59 -0500

    Update 11_Pile Pullout.py

--- pages/11_Pile Pullout.py
+++ pages/11_Pile Pullout.py
@@ -18,7 +18,6 @@ st.title("Pile Pullout — AngleOn™")
 st.caption("""
 Key Points:    
 INSTRON pile pull testing of AngleOn™ raw material.
-
 -**Average Max Load:                   33.02lbF**    
 -**Average Displacement @ Max Load:    0.121in**  
 """)
